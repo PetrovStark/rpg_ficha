8f1cfd13c213b8b4789cb754fa92c61793ce2f73
Petrus Rennan <petrus@i9me.com.br>
2019-12-27 11:54:15 -0500

    atualizando interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,171 +1,153 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: Personagens(),
-));
+      debugShowCheckedModeBanner: false,
+      home: Personagem(),
+    ));
 
-class Personagens extends StatefulWidget {
+class Personagem extends StatefulWidget {
   @override
-  _PersonagensState createState() => _PersonagensState();
+  _PersonagemState createState() => _PersonagemState();
 }
 
-class _PersonagensState extends State<Personagens> {
-
-  var charinfo = [
-    {
-      "field_name": "NOME",
-      "field_value" : "Rose Garden",
-      "icon" : Icons.person,
-    },
+class _PersonagemState extends State<Personagem> {
+  var atributos = [
+    {"field_name": "NOME", "field_value": "Keyko Amano", "icon": Icons.person},
     {
       "field_name": "CLASSE",
-      "field_value" : "Guerreira",
-      "icon": Icons.group_work,
+      "field_value": "Guerreira",
+      "icon": Icons.group_work
     },
     {
-      "field_name": "HP/MP",
-      "field_value" : "200/400",
-      "icon" : Icons.healing,
-    }
+      "field_name": "TÍTULO",
+      "field_value": "Exterminadora de Goblins",
+      "icon": Icons.brightness_7
+    },
   ];
 
-  int forca = 30;
+  int counter = 0;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
       appBar: AppBar(
+        backgroundColor: Colors.grey[850],
         title: Text("RPG"),
         centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0,
       ),
+      backgroundColor: Colors.grey[900],
       body: Padding(
-        padding: EdgeInsets.fromLTRB(20, 40, 30, 0),
+        padding: EdgeInsets.fromLTRB(20, 40, 20, 0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                radius: 70,
-                backgroundImage: AssetImage("assets/milliemolly.png"),
-              ),
-            ),
-            Divider(
-              height: 60,
-              color: Colors.grey[600],
-            ),
-            Column(
-              children: charinfo.map((info) => Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(right: 10),
-                        child: Icon(info["icon"], color: Colors.grey, size: 20),
-                      ),
-                      Text(
-                        info["field_name"],
-                        style: TextStyle(
-                          color: Colors.grey,
-                          letterSpacing: 2,
-                          fontSize: 10,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    info["field_value"],
-                    style: TextStyle(
-                      color: Colors.grey[200],
-                      letterSpacing: 2,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 30),
-                ],
-              )).toList(),
-            ),
+            //PERFIL
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(right: 10),
-                        child: Icon(Icons.fitness_center, color: Colors.grey, size: 20),
-                      ),
-                      Text(
-                        "$forca",
-                        style: TextStyle(
-                          color: Colors.grey[200],
-                          letterSpacing: 2,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
+                CircleAvatar(
+                  backgroundImage: AssetImage("assets/milliemolly.png"),
+                  radius: 50,
                 ),
-                Expanded(
-                  flex: 1,
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(right: 10),
-                        child: Icon(Icons.security, color: Colors.grey, size: 20),
-                      ),
-                      Text(
-                        "80",
-                        style: TextStyle(
-                          color: Colors.grey[200],
-                          letterSpacing: 2,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                Column(
+                  children: <Widget>[
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(right: 10),
+                          child: Icon(
+                            Icons.arrow_upward,
+                            color: Colors.grey[500],
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(right: 10),
-                        child: Icon(Icons.offline_bolt, color: Colors.grey, size: 20),
-                      ),
-                      Text(
-                        "30",
-                        style: TextStyle(
-                          color: Colors.grey[200],
-                          letterSpacing: 2,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                        Text(
+                          "LV",
+                          style: TextStyle(
+                            color: Colors.grey[500],
+                            letterSpacing: 2,
+                            fontSize: 12,
+                          ),
                         ),
+                      ],
+                    ),
+                    Text(
+                      "$counter",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ],
+            ),
+            Divider(
+              color: Colors.grey[500],
+              height: 60,
+            ),
+            //ATRIBUTOS PERSONAGEM
+            Column(
+              children: atributos
+                  .map((attr) => Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            //ATRIBUTO
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Padding(
+                                  padding: EdgeInsets.only(right: 10),
+                                  child: Icon(
+                                    attr["icon"],
+                                    color: Colors.grey[500],
+                                  ),
+                                ),
+                                Text(
+                                  attr["field_name"],
+                                  style: TextStyle(
+                                    color: Colors.grey[500],
+                                    letterSpacing: 2,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              attr["field_value"],
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            //FIM DO ATRIBUTO
+                          ],
+                        ),
+                      ))
+                  .toList(),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState((){
-            forca++;
-          });
-        },
         child: Icon(Icons.add),
         backgroundColor: Colors.grey[850],
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
       ),
     );
   }
